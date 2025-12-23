3d298494fefe1774ef9cebb2ba827ebbee7dd848
unknown <viktor.hurtig@outlook.com>
2025-12-23 13:00:25 -0800

    feat: Initial Push

---
+++ HolistiQ-Backend/holistiq-core/src/main/java/com/hurtigpayes/holistiQ/HolistiqCoreApplication.java
@@ -0,0 +1,13 @@
+package com.hurtigpayes.holistiQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolistiqCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolistiqCoreApplication.class, args);
+	}
+
+}
---
+++ HolistiQ-Backend/holistiq-core/src/test/java/com/hurtigpayes/holistiQ/HolistiqCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hurtigpayes.holistiQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolistiqCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
