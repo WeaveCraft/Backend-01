0e53c8af57beba4f127fbf24b58f449fb335fc35
unknown <viktor.hurtig@outlook.com>
2025-12-24 12:40:00 -0800

    feature: working on Account entity

--- HolistiQ-Backend/holistiq-core/src/main/java/com/hurtigpayes/holistiQ/account/entity/Account.java
+++ HolistiQ-Backend/holistiq-core/src/main/java/com/hurtigpayes/holistiQ/account/entity/Account.java
@@ -11,6 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,4 +48,9 @@ public class Account {
 
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Transaction> transactions = new ArrayList<>();
+
+    private LocalDateTime closedAt;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt;
 }
