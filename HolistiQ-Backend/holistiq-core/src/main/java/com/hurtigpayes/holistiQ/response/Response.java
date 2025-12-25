7af5eb1550126536509c6180aaeba30008033e75
unknown <viktor.hurtig@outlook.com>
2025-12-25 11:30:17 -0800

    feat: adding respone from feedback from frontend

--- HolistiQ-Backend/holistiq-core/src/main/java/com/hurtigpayes/holistiQ/response/Response.java
+++ HolistiQ-Backend/holistiq-core/src/main/java/com/hurtigpayes/holistiQ/response/Response.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+import java.util.Map;
+
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,4 +15,5 @@ public class Response<T> {
     private int statusCode;
     private String message;
     private T data;
+    private Map<String, Serializable> meta;
 }
