6c552f29fd6b2478eee7bd76cc551a1565a4d807
unknown <viktor.hurtig@outlook.com>
2025-12-24 17:13:39 -0800

    feat: working on Transaction model

---
+++ HolistiQ-Backend/holistiq-core/src/main/java/com/hurtigpayes/holistiQ/transaction/entity/Transaction.java
@@ -0,0 +1,40 @@
+package com.hurtigpayes.holistiQ.transaction.entity;
+
+import com.hurtigpayes.holistiQ.enums.TransactionStatus;
+import com.hurtigpayes.holistiQ.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@Table(name = "transaction")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TransactionType transactionType;
+
+    @Column(nullable = false)
+    private LocalDateTime transactionDate = LocalDateTime.now();
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+}
